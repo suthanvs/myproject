e877ee45174162e1965110f55a2fe56aad458aea
Suthan <suthan.venkataramana@rackspace.com>
2014-10-04 14:17:48 -0500

    first commit

---
+++ Queen.py
@@ -0,0 +1,21 @@
+import sys
+
+class Queen:
+    x = []
+
+    @staticmethod
+    def check(self,k,i):
+      for j in range(k):
+          if(x[j]==i or abs())
+
+    def queen(self):
+        #var = None
+        print("Inside")
+        #var = 'name'
+        sample = 'var'
+        v = getattr(self,sample)
+        print v()
+
+
+var= Queen()
+var.queen()
